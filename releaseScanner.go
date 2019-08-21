b3f9201e7a5f5df277e58ea92bac040ef5b07c15
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-08-21 15:16:40 +0200

    Read plugin meta data from front matter

--- releaseScanner.go
+++ releaseScanner.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"errors"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"os"
+	"path/filepath"
 )
 
 func scanDirectory(directory string) ([]Plugin, error) {
@@ -16,13 +19,52 @@ func scanDirectory(directory string) ([]Plugin, error) {
 	}
 
 	for _, pluginDirectory := range pluginDirectories {
-		println("checking", pluginDirectory)
-		plugins = append(plugins, readPluginDirectory(pluginDirectory))
+		plugin := readPluginDirectory(filepath.Join(directory, pluginDirectory.Name()))
+		if plugin != nil {
+			plugins = append(plugins, *plugin)
+		}
 	}
 
 	return plugins, nil
 }
 
-func readPluginDirectory(pluginDirectory os.FileInfo) Plugin {
-	return Plugin{name: pluginDirectory.Name()}
+func readPluginDirectory(pluginDirectory string) *Plugin {
+	indexFileName := filepath.Join(pluginDirectory, "index.md")
+	indexContent, err := ioutil.ReadFile(indexFileName)
+	if err != nil {
+		return nil
+	}
+	data, err := unmarshalFrontMatter(indexContent)
+	if err == nil {
+		return &Plugin{
+			name:        data.Name,
+			displayName: data.DisplayName,
+			description: data.Description,
+			category:    data.Category,
+			author:      data.Author,
+		}
+	} else {
+		return nil
+	}
+}
+
+type pluginFrontMatter struct {
+	Name        string `yaml:"name"`
+	DisplayName string `yaml:"displayName"`
+	Description string `yaml:"description"`
+	Category    string `yaml:"category"`
+	Author      string `yaml:"author"`
+}
+
+func unmarshalFrontMatter(b []byte) (pluginFrontMatter, error) {
+	var frontMatterDelimiter = []byte("---")
+	var data pluginFrontMatter
+
+	if !bytes.HasPrefix(b, frontMatterDelimiter) {
+		return data, errors.New("index.md file has no front matter part")
+	}
+
+	parts := bytes.SplitN(b, frontMatterDelimiter, 3)
+	err := yaml.Unmarshal(parts[1], &data)
+	return data, err
 }
--- releaseScanner_test.go
+++ releaseScanner_test.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
+func TestFailureForNotExistingPluginsFolder(t *testing.T) {
+	configuration := Configuration{DescriptorDirectory: "no/such/folder"}
+
+	_, err := scanDirectory(configuration.DescriptorDirectory)
+
+	assert.Error(t, err, "expected error missing", err)
+}
+
 func TestIfReleaseFilesAreRead(t *testing.T) {
 	configuration := Configuration{DescriptorDirectory: "resources/test/plugins"}
 
@@ -12,4 +20,26 @@ func TestIfReleaseFilesAreRead(t *testing.T) {
 
 	assert.Nil(t, err, "unexpected error reading directory", err)
 	assert.Equal(t, 3, len(plugins), "wrong number of plugins")
+}
+
+func TestIfPluginMetadataIsRead(t *testing.T) {
+	configuration := Configuration{DescriptorDirectory: "resources/test/plugins"}
+
+	plugins, _ := scanDirectory(configuration.DescriptorDirectory)
+	plugin := findPluginByName(plugins, "scm-auth-ldap-plugin")
+
+	assert.Equal(t, "scm-auth-ldap-plugin", plugin.name)
+	assert.Equal(t, "LDAP", plugin.displayName)
+	assert.Equal(t, "LDAP Authentication", plugin.description)
+	assert.Equal(t, "authentication", plugin.category)
+	assert.Equal(t, "Cloudogu GmbH", plugin.author)
+}
+
+func findPluginByName(plugins []Plugin, name string) *Plugin {
+	for _, plugin := range plugins {
+		if name == plugin.name {
+			return &plugin
+		}
+	}
+	return nil
 }
